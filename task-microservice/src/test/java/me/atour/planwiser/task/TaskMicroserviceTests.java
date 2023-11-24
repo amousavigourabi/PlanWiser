ec90edecb592d72ea8440c5aeb94efc609e80fda
Atour <28668597+amousavigourabi@users.noreply.github.com>
2023-11-24 18:32:47 +0100

    Set up Spring project

---
+++ task-microservice/src/test/java/me/atour/planwiser/task/TaskMicroserviceTests.java
@@ -0,0 +1,13 @@
+package me.atour.planwiser.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMicroserviceTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
